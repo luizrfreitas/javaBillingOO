a337e94499fc29526346830db0e30781beb28269
Luiz R Freitas <luizfreitasaca@gmail.com>
2023-09-08 23:30:41 -0300

    save

---
+++ src/model/entities/FaturaSustituicion.java
@@ -0,0 +1,27 @@
+package model.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+import model.entities.Service;
+import model.entities.Fatura;
+
+
+public class FaturaSustituicion implements Fatura {
+
+    private Cliente cliente;
+    private List<Service> services;
+
+    public FaturaSustituicion () {}
+
+    @Override
+    public Double calculateTax () {
+        return 1.0;
+    }
+
+    @Override
+    public Double calculateBill () {
+        return 1.0;
+    }
+
+
+}
